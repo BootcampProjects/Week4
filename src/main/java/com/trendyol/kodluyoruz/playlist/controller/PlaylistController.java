7b6815831efe34b91869791a210a8126a95f3d88
MustafaCEVIK <se.mustafacevik@gmail.com>
2020-09-30 21:24:58 +0300

    Refactoring PlaylistController, implementation PlaylistService.

--- src/main/java/com/trendyol/kodluyoruz/playlist/controller/PlaylistController.java
+++ src/main/java/com/trendyol/kodluyoruz/playlist/controller/PlaylistController.java
@@ -1,8 +1,9 @@
 package com.trendyol.kodluyoruz.playlist.controller;
 
 import com.trendyol.kodluyoruz.playlist.contract.request.CreatePlaylistRequest;
-import com.trendyol.kodluyoruz.playlist.contract.request.UpdatePlaylistTrackRequest;
 import com.trendyol.kodluyoruz.playlist.domain.Playlist;
+import com.trendyol.kodluyoruz.playlist.domain.Track;
+import com.trendyol.kodluyoruz.playlist.service.PlaylistService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,42 +13,46 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/playlists")
 public class PlaylistController {
+    private final PlaylistService playlistService;
+
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
 
     @PostMapping
     public ResponseEntity<Void> createPlaylist(@RequestBody CreatePlaylistRequest request) {
-        String id = "newPostId123";
-        URI location = URI.create(String.format("/playlists/%s/", id));
+        Playlist playlist = new Playlist(request.getName(), request.getDescription(), request.getUserId());
+        playlistService.createPlaylist(playlist);
+        URI location = URI.create(String.format("/playlists/%s/", playlist.getId()));
 
         return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Playlist> findPlaylistById(@PathVariable String id) {
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(playlistService.findById(id));
     }
 
     @GetMapping("/u/{userId}")
     public ResponseEntity<List<Playlist>> findAllPlaylistByUserId(@PathVariable String userId, @RequestParam(required = false, defaultValue = "0") String offset, @RequestParam(required = false, defaultValue = "5") String limit) {
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(playlistService.findAllByUserId(userId));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePlaylist(@PathVariable String id) {
-
+        playlistService.deletePlaylist(id);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}/a")
-    public ResponseEntity<Void> addTrackToPlaylist(@PathVariable String id, @RequestBody UpdatePlaylistTrackRequest request) {
-
+    public ResponseEntity<Void> addTrackToPlaylist(@PathVariable String id, @RequestBody Track track) {
+        playlistService.addTrackToPlaylist(id, track);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{id}/r")
-    public ResponseEntity<Void> removeTrackToPlaylist(@PathVariable String id, @RequestBody UpdatePlaylistTrackRequest request) {
-
+    public ResponseEntity<Void> removeTrackToPlaylist(@PathVariable String id, @RequestBody Track track) {
+        playlistService.removeTrackToPlaylist(id, track);
         return ResponseEntity.noContent().build();
     }
 }
